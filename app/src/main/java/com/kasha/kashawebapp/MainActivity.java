9002e67044d3bdc6a995e7cdcfc7155af570f218
Rob <basorot@gmail.com>
2016-10-15 01:42:22 +0200

    ADDED get the order id from the url within the webview

--- app/src/main/java/com/kasha/kashawebapp/MainActivity.java
+++ app/src/main/java/com/kasha/kashawebapp/MainActivity.java
@@ -1,8 +1,10 @@
 package com.kasha.kashawebapp;
 
 import android.annotation.SuppressLint;
+import android.annotation.TargetApi;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.ActionBar;
@@ -10,9 +12,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.KeyEvent;
 import android.view.MotionEvent;
 import android.view.View;
+import android.webkit.WebResourceError;
+import android.webkit.WebResourceRequest;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Toast;
+
+import static android.widget.Toast.LENGTH_SHORT;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -99,10 +106,10 @@ public class MainActivity extends AppCompatActivity {
         webSettings.setJavaScriptEnabled(true);
 
 
-        myWebView.getSettings().setAppCacheMaxSize( 5 * 1024 * 1024 ); // 5MB
-        myWebView.getSettings().setAppCachePath( getApplicationContext().getCacheDir().getAbsolutePath() );
+        //myWebView.getSettings().setAppCacheMaxSize( 5 * 1024 * 1024 ); // 5MB
+        //myWebView.getSettings().setAppCachePath( getApplicationContext().getCacheDir().getAbsolutePath() );
         myWebView.getSettings().setAllowFileAccess( true );
-        myWebView.getSettings().setAppCacheEnabled( true );
+        //myWebView.getSettings().setAppCacheEnabled( true );
         myWebView.getSettings().setJavaScriptEnabled( true );
         myWebView.getSettings().setCacheMode( WebSettings.LOAD_DEFAULT ); // load online by default
 
@@ -110,27 +117,65 @@ public class MainActivity extends AppCompatActivity {
             myWebView.getSettings().setCacheMode( WebSettings.LOAD_CACHE_ELSE_NETWORK );
         }
 
-        //myWebView.loadUrl("ec2-52-57-159-28.eu-central-1.compute.amazonaws.com");
         myWebView.loadUrl("http://ec2-52-57-159-28.eu-central-1.compute.amazonaws.com/");
+        //myWebView.loadUrl("http://www.google.com/");
 
         myWebView.setWebViewClient(new WebViewClient() {
+
+            @SuppressWarnings("deprecation")
             @Override
             public boolean shouldOverrideUrlLoading(WebView view, String url) {
+
+                //multiple split to ensure even if the url format change it doesn't break easily
+                String[] linkContent = url.split("\\/");
+                String[] linkContent1 = (linkContent[linkContent.length-1]). split("\\?");
+                String[] linkContent2 = (linkContent1[linkContent1.length-1]).split("=");
+                String orderKey = linkContent2[linkContent2.length-1];
+
+                if(orderKey.startsWith("wc_order_")) {
+                    Toast toast = Toast.makeText(getApplicationContext(), orderKey, LENGTH_SHORT);
+                    toast.show();
+                }
+
                 view.loadUrl(url);
                 return false;
             }
 
+            @TargetApi(Build.VERSION_CODES.N)
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                String url = request.getUrl().toString();
+
+                //multiple split to ensure even if the url format change it doesn't break easily
+                String[] linkContent = url.split("\\/");
+                String[] linkContent1 = (linkContent[linkContent.length-1]). split("\\?");
+                String[] linkContent2 = (linkContent1[linkContent1.length-1]).split("=");
+                String orderKey = linkContent2[linkContent2.length-1];
+
+                if(orderKey.startsWith("wc_order_")) {
+                    Toast toast = Toast.makeText(getApplicationContext(), orderKey, LENGTH_SHORT);
+                    toast.show();
+                }
+
+                view.loadUrl(url);
+                return true;
+            }
+
+            @SuppressWarnings("deprecation")
+            @Override
             public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
                 myWebView.loadUrl("file:///android_asset/error.html");
+            }
 
+            @TargetApi(android.os.Build.VERSION_CODES.M)
+            @Override
+            public void onReceivedError(WebView view, WebResourceRequest req, WebResourceError rerr) {
+                myWebView.loadUrl("file:///android_asset/error.html");
             }
 
         });
 
-
-
     }
-
 
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
@@ -153,10 +198,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onPostCreate(Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
-
-        // Trigger the initial hide() shortly after the activity has been
-        // created, to briefly hint to the user that UI controls
-        // are available.
         delayedHide(100);
     }
 
@@ -193,10 +234,6 @@ public class MainActivity extends AppCompatActivity {
         mHideHandler.postDelayed(mShowPart2Runnable, UI_ANIMATION_DELAY);
     }
 
-    /**
-     * Schedules a call to hide() in [delay] milliseconds, canceling any
-     * previously scheduled calls.
-     */
     private void delayedHide(int delayMillis) {
         mHideHandler.removeCallbacks(mHideRunnable);
         mHideHandler.postDelayed(mHideRunnable, delayMillis);
@@ -207,5 +244,5 @@ public class MainActivity extends AppCompatActivity {
             = (ConnectivityManager) getSystemService(this.CONNECTIVITY_SERVICE);
     NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
     return activeNetworkInfo != null && activeNetworkInfo.isConnected();
-}
+    }
 }
