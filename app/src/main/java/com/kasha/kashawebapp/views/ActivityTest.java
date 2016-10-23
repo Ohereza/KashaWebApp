46d26f38bd71bb1309b70088f485d51be48aba1b
Rene Kabagamba <rkabagam@andrew.cmu.edu>
2016-10-23 13:25:51 +0200

    LocatorService

--- app/src/main/java/com/kasha/kashawebapp/views/ActivityTest.java
+++ app/src/main/java/com/kasha/kashawebapp/views/ActivityTest.java
@@ -1,5 +1,8 @@
 package com.kasha.kashawebapp.views;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.IntentSender;
 import android.content.pm.PackageManager;
 import android.location.Location;
@@ -7,22 +10,18 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import com.franmontiel.persistentcookiejar.ClearableCookieJar;
-import com.franmontiel.persistentcookiejar.PersistentCookieJar;
-import com.franmontiel.persistentcookiejar.cache.SetCookieCache;
-import com.franmontiel.persistentcookiejar.persistence.SharedPrefsCookiePersistor;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.PendingResult;
 import com.google.android.gms.common.api.ResultCallback;
 import com.google.android.gms.common.api.Status;
-import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.location.LocationSettingsRequest;
@@ -30,29 +29,19 @@ import com.google.android.gms.location.LocationSettingsResult;
 import com.google.android.gms.location.LocationSettingsStates;
 import com.google.android.gms.location.LocationSettingsStatusCodes;
 import com.kasha.kashawebapp.R;
-import com.kasha.kashawebapp.helper.Configs;
-import com.kasha.kashawebapp.helper.LocationUpdateResponse;
-import com.kasha.kashawebapp.helper.LocationUpdater;
-import com.kasha.kashawebapp.helper.LoginResponse;
 import com.kasha.kashawebapp.interfaces.PdsAPI;
-
-import java.text.DateFormat;
-import java.util.Date;
+import com.kasha.kashawebapp.services.LocatorService;
 
 import okhttp3.OkHttpClient;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
 
 /**
  * Created by rkabagamba on 10/13/2016.
  */
 
-public class ActivityTest extends AppCompatActivity implements
-        GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener,
-        LocationListener {
+public class ActivityTest extends AppCompatActivity
+        implements GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener{
 
     private static final int REQUEST_ACCESS_FINE_LOCATION = 0;
     private GoogleApiClient mGoogleApiClient = null;
@@ -65,7 +54,9 @@ public class ActivityTest extends AppCompatActivity implements
     private String mLastUpdateTime;
 
     private LocationRequest mLocationRequest;
+
     private Boolean mRequestingLocationUpdates = true;
+
 
 
     private Button sendToServerButton;
@@ -86,14 +77,47 @@ public class ActivityTest extends AppCompatActivity implements
         mLastUpdateTimeTextView = (EditText) findViewById(R.id.last_update_time);
         sendToServerButton = (Button) findViewById(R.id.send_button);
 
+
+/*        // Get Location Manager and check for GPS & Network location services
+        LocationManager lm = (LocationManager) getSystemService(LOCATION_SERVICE);
+        if(!lm.isProviderEnabled(LocationManager.GPS_PROVIDER) ||
+                !lm.isProviderEnabled(LocationManager.NETWORK_PROVIDER)) {*/
+
+            // Build the alert dialog
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("NOTICE");
+            builder.setMessage("Using the Premium Delivery service will enable GPS " +
+                    "and other Location Services. " +
+                    "This in order to collect your location and deliver your package where you are. ");
+
+            builder.setNegativeButton("Disagree", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    promptDropOffPoint();
+                }
+            });
+            builder.setPositiveButton("Agree", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    trackUserLocation();
+                }
+            });
+
+            Dialog alertDialog = builder.create();
+            alertDialog.setCanceledOnTouchOutside(false);
+            alertDialog.show();
+       // }
+
+
+    }
+
+    protected void trackUserLocation(){
+
+        // Programmatically request ACCESS_FINE_LOCATION PERMISSION if missing
         int permissionCheck = ContextCompat.checkSelfPermission(getApplicationContext(),
                 android.Manifest.permission.ACCESS_FINE_LOCATION);
-
         if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
-
-            ActivityCompat.requestPermissions(this,
+            ActivityCompat.requestPermissions(getParent(),
                     new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
-                                                            REQUEST_ACCESS_FINE_LOCATION);
+                    REQUEST_ACCESS_FINE_LOCATION);
         }
 
         // Create an instance of GoogleAPIClient.
@@ -105,60 +129,15 @@ public class ActivityTest extends AppCompatActivity implements
                     .build();
         }
 
+        mGoogleApiClient.connect();
         createLocationRequest();
 
-        sendToServerButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                cookieJar = new PersistentCookieJar(new SetCookieCache(),
-                        new SharedPrefsCookiePersistor(getApplicationContext()));
-                okHttpClient = new OkHttpClient.Builder()
-                        .cookieJar(cookieJar)
-                        .build();
-
-                retrofit = new Retrofit.Builder()
-                        .baseUrl(Configs.serverAddress)
-                        .addConverterFactory(GsonConverterFactory.create())
-                        .client(okHttpClient)
-                        .build();
-
-                pdsAPI = retrofit.create(PdsAPI.class);
-
-                pdsAPI.login("Administrator", "pds").enqueue(new Callback<LoginResponse>() {
-                    @Override
-                    public void onResponse(Call<LoginResponse> call,
-                                           Response<LoginResponse> response){
-
-                        pdsAPI.updateLocation( new LocationUpdater("kasha899","Client",
-                                        String.valueOf(mLastLocation.getLongitude()),
-                                        String.valueOf(mLastLocation.getLatitude()))).enqueue(
-                                new Callback<LocationUpdateResponse>() {
-                                    @Override
-                                    public void onResponse(Call<LocationUpdateResponse> call,
-                                                           Response<LocationUpdateResponse> response){
-                                        Toast.makeText(getApplicationContext(),"Posting successfull",
-                                                Toast.LENGTH_LONG).show();
-                                    }
-
-                                    @Override
-                                    public void onFailure(Call<LocationUpdateResponse> call, Throwable t){
-                                        Toast.makeText(getApplicationContext(),"Posting unsuccessfull",
-                                                Toast.LENGTH_LONG).show();
-                                    }
-                                }
-                        );
-                    }
-
-                    @Override
-                    public void onFailure(Call<LoginResponse> call, Throwable t) {
-
-                    }
-                });
-
-            }
-        });
+        // Locator service testing:
+        Intent locationServiceIntent = new Intent(this,LocatorService.class);
+        startService(locationServiceIntent);
     }
+
+
 
     protected void createLocationRequest() {
         mLocationRequest = new LocationRequest();
@@ -188,6 +167,7 @@ public class ActivityTest extends AppCompatActivity implements
                         try{
                             status.startResolutionForResult(ActivityTest.this,
                                     1);
+
                         }catch (IntentSender.SendIntentException e) {
                             // Ignore the error.
                         }
@@ -200,41 +180,50 @@ public class ActivityTest extends AppCompatActivity implements
 
     }
 
+    protected void promptDropOffPoint(){
+        Toast.makeText(getApplicationContext(),"Prompt drop-off point", Toast.LENGTH_LONG).show();
 
-    @Override
+    }
+
+   @Override
     public void onConnected(Bundle connectionHint) {
         int permissionCheck = ContextCompat.checkSelfPermission(getApplicationContext(),
                 android.Manifest.permission.ACCESS_FINE_LOCATION);
         if (permissionCheck == PackageManager.PERMISSION_GRANTED) {
             mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
                     mGoogleApiClient);
-                if (mLastLocation != null) {
+/*                if (mLastLocation != null) {
                     mLatitudeTextView.setText(String.valueOf(mLastLocation.getLatitude()));
                     mLongitudeTextView.setText(String.valueOf(mLastLocation.getLongitude()));
-                }
+                }*/
         }
 
-        if (mRequestingLocationUpdates) {
+/*        if (mRequestingLocationUpdates) {
             startLocationUpdates();
-        }
+        }*/
     }
 
-    protected void startLocationUpdates() {
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+/*    protected void startLocationUpdates() {
         int permissionCheck = ContextCompat.checkSelfPermission(getApplicationContext(),
                 android.Manifest.permission.ACCESS_FINE_LOCATION);
         if (permissionCheck == PackageManager.PERMISSION_GRANTED) {
             LocationServices.FusedLocationApi.requestLocationUpdates(
                     mGoogleApiClient, mLocationRequest, this);
         }
-    }
+    }*/
 
 
-    @Override
+/*    @Override
     public void onLocationChanged(Location location) {
         mCurrentLocation = location;
         mLastUpdateTime = DateFormat.getTimeInstance().format(new Date());
         updateUI();
-    }
+    }*/
 
     private void updateUI() {
         mLatitudeTextView.setText(String.valueOf(mCurrentLocation.getLatitude()));
@@ -242,26 +231,19 @@ public class ActivityTest extends AppCompatActivity implements
         mLastUpdateTimeTextView.setText(mLastUpdateTime);
     }
 
-
-
     protected void onStart() {
-        mGoogleApiClient.connect();
+        //mGoogleApiClient.connect();
         super.onStart();
     }
 
     protected void onStop() {
-        mGoogleApiClient.disconnect();
+        //mGoogleApiClient.disconnect();
         super.onStop();
     }
 
 
     @Override
-    public void onConnectionSuspended(int i) {
-
-    }
-
-    @Override
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-    }
 
+    }
 }
