cc3a2632882c81deef5c82d5bf24374e1ba2e97e
Rene Kabagamba <rkabagam@andrew.cmu.edu>
2016-10-23 21:23:55 +0200

    BroadcastReceivers package

---
+++ app/src/main/java/com/kasha/kashawebapp/BroadcastReceivers/OnBootReceiver.java
@@ -0,0 +1,17 @@
+package com.kasha.kashawebapp.BroadcastReceivers;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class OnBootReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        if (intent.getAction().equals("android.intent.action.BOOT_COMPLETED")) {
+            // on boot restart locator service
+
+        }
+    }
+}
