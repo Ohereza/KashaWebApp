9089d642f8ef183f7ea210d718d5c660680bb0ba
Rene Kabagamba <rkabagam@andrew.cmu.edu>
2016-11-20 20:39:28 +0200

    pubnub background service

--- app/src/main/java/com/kasha/kashawebapp/services/MyPubnubListenerService.java
+++ app/src/main/java/com/kasha/kashawebapp/services/MyPubnubListenerService.java
@@ -36,30 +36,6 @@ import static com.kasha.kashawebapp.helper.Configs.PREFS_NAME;
  * Created by rkabagamba on 11/2/2016.
  */
 
-/* public class MyPubnubListenerService extends SubscribeCallback {
-    private static final String TAG_PUBNUB = "pubnub";
-
-    @Override
-    public void status(PubNub pubnub, PNStatus status) {
-        // for common cases to handle, see: https://www.pubnub.com/docs/java/pubnub-java-sdk-v4
-    }
-    @Override
-    public void message(PubNub pubnub, PNMessageResult message) {
-        Log.v(TAG_PUBNUB, "OnChannel: "+message.getChannel()+" message("+message.getMessage()+ ")");
-
-        if (message.getChannel().toString().equalsIgnoreCase("deliveryRequests")){
-            Log.v(TAG_PUBNUB, "On delivery requests handling");
-
-        }else if(message.getChannel().toString().equalsIgnoreCase("mymaps")){
-
-        }
-    }
-
-    @Override
-    public void presence(PubNub pubnub, PNPresenceEventResult presence) {
-        // no presence handling for simplicity
-    }
-}*/
 
 public class MyPubnubListenerService extends IntentService {
 
