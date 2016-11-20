ad8099464d6f489ba07fed5e6454bf8c2a911c76
Rob <basorot@gmail.com>
2016-11-20 19:32:23 +0200

    CLEANUP of comments
    Signed-off-by: Rob <basorot@gmail.com>

--- app/src/main/java/com/kasha/kashawebapp/fragments/HomeFragment.java
+++ app/src/main/java/com/kasha/kashawebapp/fragments/HomeFragment.java
@@ -42,15 +42,8 @@ import static com.kasha.kashawebapp.helper.Configs.PREFS_NAME;
  * create an instance of this fragment.
  */
 public class HomeFragment extends Fragment {
-        // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
     private final String webUrl = "http://ec2-52-29-162-226.eu-central-1.compute.amazonaws.com/";
-
-    private String mParam1;
-    private String mParam2;
 
     private View rootView;
     private WebView kWebView;
@@ -73,8 +66,6 @@ public class HomeFragment extends Fragment {
     public static HomeFragment newInstance(String param1, String param2) {
         HomeFragment fragment = new HomeFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -101,22 +92,15 @@ public class HomeFragment extends Fragment {
         WebSettings webSettings = kWebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
 
-
-        //myWebView.getSettings().setAppCacheMaxSize( 5 * 1024 * 1024 ); // 5MB
-        //myWebView.getSettings().setAppCachePath( getApplicationContext().getCacheDir().getAbsolutePath() );
         kWebView.getSettings().setAllowFileAccess( true );
-        //myWebView.getSettings().setAppCacheEnabled( true );
         kWebView.getSettings().setJavaScriptEnabled( true );
-        kWebView.getSettings().setCacheMode( WebSettings.LOAD_DEFAULT ); // load online by default
+        kWebView.getSettings().setCacheMode( WebSettings.LOAD_DEFAULT );
 
         if (!isNetworkAvailable()) { // loading offline
             kWebView.getSettings().setCacheMode( WebSettings.LOAD_CACHE_ELSE_NETWORK );
             }
 
-        //myWebView.loadUrl("http://ec2-52-57-159-28.eu-central-1.compute.amazonaws.com/");
         kWebView.loadUrl(webUrl);
-
-
         promptUserToEnableGPS();
 
         kWebView.setWebViewClient(new WebViewClient() {
@@ -290,18 +274,6 @@ public class HomeFragment extends Fragment {
                     REQUEST_ACCESS_FINE_LOCATION);
         }
 
-        // Create an instance of GoogleAPIClient.
-//        if (mGoogleApiClient == null) {
-//            mGoogleApiClient = new GoogleApiClient.Builder(getContext())
-//                    .addConnectionCallbacks(this)
-//                    .addOnConnectionFailedListener(this)
-//                    .addApi(LocationServices.API)
-//                    .build();
-//        }
-//
-//        mGoogleApiClient.connect();
-        //createLocationRequest();
-
         Toast.makeText(getActivity(),"Start locator service",
                 Toast.LENGTH_LONG).show();
         // Locator service:
@@ -316,46 +288,6 @@ public class HomeFragment extends Fragment {
 
     }
 
-//    protected void createLocationRequest() {
-//
-//        mLocationRequest = new LocationRequest();
-//        mLocationRequest.setInterval(10000);
-//        mLocationRequest.setFastestInterval(5000);
-//        //mLocationRequest.setSmallestDisplacement(100);
-//        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-//
-//        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
-//                .addLocationRequest(mLocationRequest);
-//
-//        final PendingResult<LocationSettingsResult> result =
-//                LocationServices.SettingsApi.checkLocationSettings(mGoogleApiClient,
-//                        builder.build());
-//
-//        result.setResultCallback(new ResultCallback<LocationSettingsResult>() {
-//            @Override
-//            public void onResult(@NonNull LocationSettingsResult locationSettingsResult) {
-//                final Status status = locationSettingsResult.getStatus();
-//                final LocationSettingsStates locationSettingsStates
-//                        = locationSettingsResult.getLocationSettingsStates();
-//
-//                switch (status.getStatusCode()){
-//                    case LocationSettingsStatusCodes.SUCCESS:
-//                        break;
-//                    case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
-//                        try{
-//                            status.startResolutionForResult(getActivity(),1);
-//
-//                        }catch (IntentSender.SendIntentException e) {
-//                            // Ignore the error.
-//                        }
-//                        break;
-//                    case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
-//                        break;
-//                }
-//            }
-//        });
-//
-//    }
 
     protected void promptDropOffPoint(){
         Log.d(TAG, "Prompt drop-off point");
@@ -390,140 +322,12 @@ public class HomeFragment extends Fragment {
             alertDialog.show();
         }
     }
-
-//    @Override
-//    public boolean onKeyDown(int keyCode, KeyEvent event) {
-//        if (event.getAction() == KeyEvent.ACTION_DOWN) {
-//            switch (keyCode) {
-//                case KeyEvent.KEYCODE_BACK:
-//                    if (kWebView.canGoBack()) {
-//                        kWebView.goBack();
-//                    } else {
-//                        finish();
-//                    }
-//                    return true;
-//            }
-//
-//        }
-//        return super.onKeyDown(keyCode, event);
-//    }
-
-
-//    @Override
-//    protected void onPostCreate(Bundle savedInstanceState) {
-//        super.onPostCreate(savedInstanceState);
-//        delayedHide(100);
-//    }
-
-
-//    private void toggle() {
-//        if (kWebView) {
-//            hide();
-//        } else {
-//            show();
-//        }
-//    }
-
-//    private void hide() {
-//        // Hide UI first
-//        ActionBar actionBar = getSupportActionBar();
-//        if (actionBar != null) {
-//            actionBar.hide();
-//        }
-//        mVisible = false;
-//
-//        // Schedule a runnable to remove the status and navigation bar after a delay
-//        mHideHandler.removeCallbacks(mShowPart2Runnable);
-//        mHideHandler.postDelayed(mHidePart2Runnable, UI_ANIMATION_DELAY);
-//    }
-
-//    @SuppressLint("InlinedApi")
-//    private void show() {
-//        // Show the system bar
-//        mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-//                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION);
-//        mVisible = true;
-//
-//        // Schedule a runnable to display UI elements after a delay
-//        mHideHandler.removeCallbacks(mHidePart2Runnable);
-//        mHideHandler.postDelayed(mShowPart2Runnable, UI_ANIMATION_DELAY);
-//    }
-
-//    private void delayedHide(int delayMillis) {
-//        mHideHandler.removeCallbacks(mHideRunnable);
-//        mHideHandler.postDelayed(mHideRunnable, delayMillis);
-//    }
-
+    
     private  boolean isNetworkAvailable() {
         ConnectivityManager connectivityManager
                 = (ConnectivityManager) getActivity().getSystemService(getActivity().CONNECTIVITY_SERVICE);
         NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
         return activeNetworkInfo != null && activeNetworkInfo.isConnected();
     }
-
-//    @Override
-//    public void onConnected(@Nullable Bundle bundle) {
-//        int permissionCheck = ContextCompat.checkSelfPermission(getContext(),
-//                android.Manifest.permission.ACCESS_FINE_LOCATION);
-//        if (permissionCheck == PackageManager.PERMISSION_GRANTED) {
-//            mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
-//                    mGoogleApiClient);
-//        }
-//
-//    }
-//
-//    @Override
-//    public void onConnectionSuspended(int i) {
-//
-//    }
-//
-//    @Override
-//    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-//
-//    }
-//}
-
-
-
-///////////////////////////////////////////////////////////////////////////////////////////////////////
-    }
-
-//    // TODO: Rename method, update argument and hook method into UI event
-//    public void onButtonPressed(Uri uri) {
-//        if (mListener != null) {
-//            mListener.onFragmentInteraction(uri);
-//        }
-//    }
-
-//    @Override
-//    public void onAttach(Context context) {
-//        super.onAttach(context);
-//        if (context instanceof OnFragmentInteractionListener) {
-//            mListener = (OnFragmentInteractionListener) context;
-//        } else {
-//            throw new RuntimeException(context.toString()
-//                    + " must implement OnFragmentInteractionListener");
-//        }
-//    }
-//
-//    @Override
-//    public void onDetach() {
-//        super.onDetach();
-//        mListener = null;
-//    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-//    public interface OnFragmentInteractionListener {
-//        // TODO: Update argument type and name
-//        void onFragmentInteraction(Uri uri);
-//    }
+}
 
